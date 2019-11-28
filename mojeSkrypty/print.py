0ecd716542e1b86741ff41033361fd7a5753a50a
rafal <ra88fi@o2.pl>
2019-11-28 13:07:43 +0100

    zadania z print

--- mojeSkrypty/print.py
+++ mojeSkrypty/print.py
@@ -1,1 +1,10 @@
-print("jaka jest twoja nazwa?")
+print('TVP1', 'TVP2', 'TVN', 'Polsat', 'BBC', 'HBO', 'MTV')
+print('TVP1', 'TVP2', 'TVN', 'Polsat', 'BBC', 'HBO', 'MTV', sep=';')
+print('I like computers', 'TVP1', 'TVP2', 'TVN', 'Polsat', 'BBC', 'HBO', 'MTV', sep=' but better is ')
+
+programName = 'BBC'
+item = 'News'
+time = '18.00'
+
+print('I like watching', item, 'at', time, 'on', programName, '.') #here's a space after programName
+print('I like watching', item, 'at', time, 'on', programName + '.') #here's no space after programName
